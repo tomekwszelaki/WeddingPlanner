90c0906eff6ebdb26f095a883e6454666117a641
Tomasz Wszelaki <tomekwszelaki@gmail.com>
2014-04-28 15:20:55 +0200

    gallery and upload tabs. Https redirection

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,7 @@
 
 
 var WeddingPlanner = angular.module('WeddingPlanner', [
+    'flow',
     'ngRoute',
     'controllers'
 ]);
@@ -26,7 +27,24 @@ WeddingPlanner.config(function($routeProvider) {
             templateUrl: 'views/contact.html',
             controller: 'ContactPageCtrl'
         }).
+        when('/upload', {
+            templateUrl: 'views/upload.html',
+            controller: 'UploadCtrl'
+        }).
         otherwise({
             redirectTo: '/'
         });
 });
+
+WeddingPlanner.config(['flowFactoryProvider', function (flowFactoryProvider) {
+    flowFactoryProvider.defaults = {
+        target: '/BoxUploadThingamajig',
+        permanentErrors:[404, 500, 501]
+    };
+    // You can also set default events:
+    flowFactoryProvider.on('catchAll', function (event) {
+      console.log('catchAll', arguments);
+    });
+    // Can be used with different implementations of Flow.js
+    // flowFactoryProvider.factory = fustyFlowFactory;
+}]);
